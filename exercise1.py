3dfa37892790b27de580fff0963f0c7abfc596b8
hanktutt <35299445+hanktutt@users.noreply.github.com>
2018-02-17 12:27:20 -0500

    Exercises pygame

--- exercise1.py
+++ exercise1.py
@@ -14,24 +14,24 @@ I will do the first two lines for you as an example
 import sys, pygame	# imports the sys and pygame modules so they can be used in this program
 assert sys.version_info >= (3,4), 'This script requires at least Python 3.4' # requires that the Python 3.4 (or higher version) interpreter is being used; i.e., not compatible with Python 2
 
-screen_size = (800,600)
-FPS = 60
+screen_size = (800,600) #It creates a variable for the window size of our program using a tuple
+FPS = 60 #We create a variable Frames per Second that has a set number of 60
 
-def main():
-	pygame.init()
-	screen = pygame.display.set_mode(screen_size)
-	clock = pygame.time.Clock()
+def main(): #We are creating a new function called main
+	pygame.init() #a new command that initizalize all imported pygame modules
+	screen = pygame.display.set_mode(screen_size) #a variable that sets the pygame screen to be our previouis variable screen_size
+	clock = pygame.time.Clock()# create a variable clock that keeps track of the framerate of our pygame
 
-	while True:
-		clock.tick(FPS)
-		screen.fill((0,0,0))
+	while True:#we create an infinite loop until it is set to False
+		clock.tick(FPS)#This command keeps our cpu from running faster than the FPS we set earlier of 60.
+		screen.fill((0,0,0))#This will fill the screen black
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit(0)
+		for event in pygame.event.get():#create a set numbered loop for an event in PyGame
+			if event.type == pygame.QUIT:#an if statement that says if the event is QUIT
+				pygame.quit()#quit pygame if the event is QUIT
+				sys.exit(0)#exit the system if the event is QUIT
 
-		pygame.display.flip()
+		pygame.display.flip()#this will do something to your computers memory cauing it to print random colors in the pixels
 
-if __name__ == '__main__':
-	main()
+if __name__ == '__main__':#the _name_ is a built in variable in python, and we sre setting it equivilant to our main function
+	main()#this will run the main function
